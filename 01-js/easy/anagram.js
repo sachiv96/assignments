431f9b1707f5b69a33d510411e3519a66e39dde8
sachiv96 <24f2003515@ds.study.iitm.ac.in>
2025-05-24 13:38:53 +0530

    completed anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,27 @@
 */
 
 function isAnagram(str1, str2) {
+ a=str1.split('').sort().join('')
+ b=str2.split('').sort().join('')
+
+if(a===b){
+  return true
+}
+else{
+  return false 
+}
+
+
+
+
 
 }
 
+console.log(isAnagram("akki","ikka"))
+
+
+
+
 module.exports = isAnagram;
+
+
